29ee034dd04222ebec09d21c5fcc7efe30bd3987
joostyn <justyn.helgeson@gmail.com>
2022-08-14 18:08:41 -0500

    Installed dependencies, started handlebars pages

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
+const { engine } = require('express-handlebars');
+const { escapeRegExp } = require('lodash');
+require('dotenv').config();
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(express.static(path.join('front')));
+app.engine('hbs', engine({ extname:'.hbs' }));
+app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, () => console.log(`Listening on ${{PORT}}`));
+app.get('/', (req, res) => {
+    res.render('index', { })
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
