94526b83a882d1beff130e311450894c0edc9843
joostyn <justyn.helgeson@gmail.com>
2022-08-17 04:50:42 -0500

    Added model validation

--- models/User.js
+++ models/User.js
@@ -5,15 +5,24 @@ class User extends Model { }
 User.init({
     username: {
         type: Datatypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: 2
+        }
     },
     email: {
         type: Datatypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
     },
     password: {
         type: Datatypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: 6
+        }
     }
 }, {
     sequelize: requre('../config/db_connection'),
